1a8e50e8e520091cc9e72c0484952d53810f2ecb
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2023-12-13 11:26:18 -0500

    debugged gpt api response, needs more work on prompt engineering to get unique responses on data

--- backend/api_apps/views.py
+++ backend/api_apps/views.py
@@ -2,42 +2,32 @@ import os
 from openai import OpenAI
 from rest_framework.views import APIView
 from rest_framework.response import Response
-import requests
 from requests_oauthlib import OAuth1
 from myapi.settings import env
 import logging
-import pprint
 from rest_framework import status
 import json
 from django.core.exceptions import ImproperlyConfigured
+import requests
 
-# Create your views here.
 
+logger = logging.getLogger(__name__)
 
 class Noun_Project(APIView):
     def get(self, request, subject):
         auth = OAuth1(env.get("NOUN_API_KEY"), env.get("NOUN_SECRET_KEY"))
         endpoint = f"https://api.thenounproject.com/v2/icon?query={subject}"
-        # endpoint = "https://api.thenounproject.com/v2/icon?query=c#"
 
         response = requests.get(endpoint, auth=auth)
         responseJSON = response.json()
-        pp = pprint.PrettyPrinter(indent=2, depth=2)
-        pp.pprint(responseJSON)
         return Response(responseJSON["icons"][0]["thumbnail_url"])
-
-
-logger = logging.getLogger(__name__)
-
 
 class Gptai_Api(APIView):
     def post(self, request):
         try:
             api_key = env.get("OPENAI_API_KEY")
             if not api_key:
-                raise ImproperlyConfigured(
-                    "OPENAI_API_KEY is not set in the environment."
-                )
+                raise ImproperlyConfigured("OPENAI_API_KEY is not set in the environment.")
 
             client = OpenAI(api_key=api_key)
 
@@ -54,76 +44,170 @@ class Gptai_Api(APIView):
                 model="gpt-3.5-turbo",
                 messages=[
                     {
+                        "role": "system",
+                        "content": """
+                        you are a nurse, fluent in medical terminology and identifying trends in a patient's status and recommending care plans for nurses, you create summaries of data for assisting nurses give patient turnover reports. here is an example of a prompt and a response DO NOT SEND THIS BACK AS A RESPONSE, CHANGE THE RESPONSE BASED ON THE ASSESSMENT DATA I SEND YOU.
+                        ###Example###
+                        prompt: {
+                            encounter_diagnosis:"subdural hematoma"
+                            0:{
+                                "encounter": 1, 
+                                "patient": 1,
+                                "provider": 1,    
+                                "assessment_time": "2023-12-06T12:45:00",
+                                "neuro": "alert and oriented x4, pearrla, pupils 3mm, moves all extremities, sensation intact, speech clear",
+                                "cardio": "heart sounds s1s2 audible, cappilary refill < 3 seconds, pulses +2 all extremities, tachycardic, normotensive",
+                                "respiratory": "lungs clear to auscultation, left lower lobe crackles",
+                                "gastrointestinal": "Bowel sounds active x4 quadrants, 1 bowelmovement at 1130, diet NPO",
+                                "genitourinary": "foley catheter, urine output dark yellow at 30ml/hr"
+                            },
+                            1:{
+                                "encounter": 1,
+                                "patient": 1,
+                                "provider": 1,
+                                "assessment_time": "2023-12-06T13:30:00",
+                                "neuro": "alert and oriented x2, pearrla, pupils 3mm, moves all extremities, sensation intact, speech muffled",
+                                "cardio": "heart sounds s1s2 audible, cappilary refill < 3 seconds, pulses +2 all extremities, tachycardic, normotensive",
+                                "respiratory": "lungs clear to auscultation, left lower lobe crackles",
+                                "gastrointestinal": "Bowel sounds active x4 quadrants, diet NPO",
+                                "genitourinary": "foley catheter, urine output dark yellow at 50ml/hr"
+                            }
+                            2:{
+                                "encounter": 1,
+                                "patient": 1,
+                                "provider": 1,
+                                "assessment_time": "2023-12-06T15:30:00",
+                                "neuro": "alert and oriented x4, pearrla, pupils 3mm, moves all extremities, sensation intact, speech clear",
+                                "cardio": "heart sounds s1s2 audible, cappilary refill < 3 seconds, pulses +2 all extremities, tachycardic, normotensive",
+                                "respiratory": "lungs clear to auscultation, left lower lobe crackles",
+                                "gastrointestinal": "Bowel sounds active x4 quadrants, diet NPO",
+                                "genitourinary": "foley catheter, urine output dark yellow at 50ml/hr"
+                            }
+                        }
+                        sample good response:
+                            "neuro": {
+                                "summary": "patient's mental alertness changed from alert and oriented x4-2 at 1330 but returned to a&0x4 at the 1530 assessment",
+                                "data": {
+                                    "cognition": "alert to person and place but not situation and time during the 1530 assessment",
+                                    "cranial nerves": "intact",
+                                    "motor function": "patient moves all extremities at every assessement",
+                                    "sensation": "remained intact during the shift",
+                                    "reflexes": "cough & gag reflex pressent"
+                                }               
+                            },
+                            "cardiac": {
+                                "summary": "patient remained normotensive, with heart rate within normal range during the last 3 assessments",
+                                "data": {
+                                    "heart rate": "remained between 80-100bpm through the shift",
+                                    "blood pressure": "between 120-140/80-85mmhg",
+                                    "heart rhythm": "normal sinus", 
+                                    "edema": "no edema present"
+                                }
+                            },
+                            "respiratory": {
+                                "summary": "patients respiratory status remained stable though the shift, patient is on 3lpm of oxygen though a nasal canula, breathing is unlabored and patient has maintained oxygen saturations above 92% the entire shift",
+                                "data": {
+                                    "respiratory rate": "between 12-20bpm",
+                                    "oxygen saturation": "92-100%",
+                                    "lung sounds": "clear to auscultation in all lobes",
+                                    "cough": "none present"
+                                }
+                            },
+                            "GI": {
+                                "summary": "Throughout the shift, bowel function remained normal",
+                                "data": {
+                                    "bowel sounds": "bowel sounds remained present throughout the shift",
+                                    "appetite": "normal apetite through out the shift",
+                                    "abdominal pain": "No abdominal pain reported throughout the shift",
+                                    "bowel movements": "prior to admission"
+                                }
+                            },
+                            "GU": {
+                                "summary": "patient maintained adequate urine output throughout the shift with a foley catheter, urine remained dark yellow",
+                                "data": {
+                                    "urine output": "between 30-50ml/hr",
+                                    "voiding without difficulty": true,
+                                    "no urinary retention": true,
+                                    "no urinary incontinence": "unable to assess"
+                                },
+                            },
+                            "careplan Recommendation": "ensure neuro assessments are completed at scheduled intervals and maintain a quiet dark environment during the night and a bright environment during the day to promote adequate rest and to prevent ICU delirium"
+                        }
+                        """,
+                    },
+                    {
                         "role": "user",
                         "content": """
-    ###instruction###
-    you are here to assist medical professionals in patient handoff. 
-    summarize the given data following the example format provided.
-    DO NOT preface the response with "here is a summary of" or "from the data provided."
+                        ###instruction###
+                        you are here to assist medical professionals in patient handoff. 
+                        summarize the given data following the example format provided.
+                        DO NOT preface the response with "here is a summary of" or "from the data provided."
+                        DO NOT just respond with the example given in the system message
 
-    ###Example###
-    here is the format I want the response:
-
-    "neuro": {
-        "summary": "(summarize the nuero assessment data from the prompt passed below in a couple sentences)",
-        "data": {
-            "cognition": (summarize the information from the prompt),
-            "cranial nerves": (summarize the information from the prompt),
-            "motor function": (summarize the information from the prompt),
-            "sensation": (summarize the information from the prompt),
-            "reflexes": (summarize the information from the prompt)
-        }
-    },
-    "cardiac": {
-        "summary": "(summarize the cardiac assessments data from the prompt passed below in a couple sentences)",
-        "data": {
-            "heart rate": (summarize the information from the prompt),
-            "blood pressure": (summarize the information from the prompt),
-            "heart rhythm": (summarize the information from the prompt),
-            "edema": (summarize the information from the prompt)
-        }
-    },
-    "respiratory": {
-        "summary": "(summarize the respiratory assessment data from the prompt passed below in a couple sentences)",
-        "data": {
-            "respiratory rate": (summarize the information from the prompt),
-            "oxygen saturation": (summarize the information from the prompt),
-            "lung sounds": (summarize the information from the prompt),
-            "cough": (summarize the information from the prompt)
-        }
-    },
-    "GI": {
-        "summary": "(summarize the GI assessment data from the prompt passed below in a couple sentences)",
-        "data": {
-            "bowel sounds": (summarize the information from the prompt),
-            "appetite": (summarize the information from the prompt),
-            "abdominal pain": (summarize the information from the prompt),
-            "bowel movements": (summarize the information from the prompt)
-        }
-    },
-    "GU": {
-        "summary": "(summarize the GU assessment data from the prompt passed below in a couple sentences)",
-        "data": {
-            "urine output": (summarize the information from the prompt),
-            "voiding without difficulty": (summarize the information from the prompt),
-            "no urinary retention": (summarize the information from the prompt),
-            "no urinary incontinence":(summarize the information from the prompt)
-        }
-    },
-    "careplan Recommendation": "(create one nursing careplan recommendation taking into consideration the patient's assessment data and the diagnosis from the encounter data, response should be less than 2 sentences)"
-    }
-
-    only analyze this data: "{prompt}"
-
-    """,
+                        ###Example###
+                        here is the format that the response has to be:
+                        {
+                            "neuro":{
+                                "summary": (summarize the neuro assessment data from the prompt passed below in a couple sentences),
+                                "data": {
+                                    "cognition": (summarize the information from the prompt),
+                                    "cranial nerves": (summarize the information from the prompt),
+                                    "motor function": (summarize the information from the prompt),
+                                    "sensation": (summarize the information from the prompt),
+                                    "reflexes": (summarize the information from the prompt)
+                                }               
+                            },
+                            "cardiac": {
+                                "summary": (summarize the cardiac assessments data from the prompt passed below in a couple sentences),
+                                "data": {
+                                    "heart rate": (summarize the information from the prompt),
+                                    "blood pressure": (summarize the information from the prompt),
+                                    "heart rhythm": (summarize the information from the prompt),
+                                    "edema": (summarize the information from the prompt)
+                                }
+                            },
+                            "respiratory": {
+                                "summary": (summarize the respiratory assessment data from the prompt passed below in a couple sentences)"
+                                "data": {
+                                    "respiratory rate": (summarize the information from the prompt),
+                                    "oxygen saturation": (summarize the information from the prompt),
+                                    "lung sounds": (summarize the information from the prompt),
+                                    "cough": (summarize the information from the prompt)
+                                }
+                            },
+                            "GI": {
+                                "summary": (summarize the GI assessment data from the prompt passed below in a couple sentences),
+                                "data": {
+                                    "bowel sounds": (summarize the information from the prompt),
+                                    "appetite": (summarize the information from the prompt),
+                                    "abdominal pain": (summarize the information from the prompt),
+                                    "bowel movements": (summarize the information from the prompt)
+                                }
+                            },
+                            "GU": {
+                                "summary": (summarize the GU assessment data from the prompt passed below in a couple sentences),
+                                "data": {
+                                    "urine output": (summarize the information from the prompt),
+                                    "voiding without difficulty": (summarize the information from the prompt),
+                                    "no urinary retention": (summarize the information from the prompt),
+                                    "no urinary incontinence": (summarize the information from the prompt)
+                                },
+                            },
+                            "careplan": (create one nursing careplan recommendation taking into consideration the patient's assessment data and the diagnosis from the encounter data, response should be less than 2 sentences)
+                        }
+                        }
+                        use this data to complete the request: {prompt}
+                        """,
                     },
                 ],
             )
-            print(prompt)
+
             response_content = completion.choices[0].message.content
+            print("Response Content:", response_content)
             response_dict = json.loads(response_content)
-            print(response_content)
+
             return Response({"response_content": response_dict})
+
         except Exception as e:
             # Log the exception
             logger.error(f"An error occurred: {str(e)}")
--- frontend/src/components/GetAssessments.jsx
+++ frontend/src/components/GetAssessments.jsx
@@ -1,19 +1,14 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Cards from "../components/Card";
 import { api } from "../utilities";
 import Card from "react-bootstrap/Card";
-import { useNavigate } from "react-router-dom";
-import Button from "react-bootstrap/Button";
+
 // getting all assessments by encounter ID
 const Assessments = () => {
-  const [patient, setPatient] = useState({});
   const [loading, setLoading] = useState(true);
   const [summarized, setSummarized] = useState("");
   const [assessments, setAssessments] = useState({});
   const [error, setError] = useState(null);
-  const navigate = useNavigate();
   const { id } = useParams();
 
   const getAssessments = async () => {
@@ -25,10 +20,11 @@ const Assessments = () => {
     // console.log(headers)
     try {
       setLoading(true);
+      // console.log(JSON.stringify({ prompt: { assessments } }));
       const response = await api.get(`v1/assessments/encounter/${id}/`, {
         headers,
       });
-      console.log(response.data);
+      // console.log(response.data);
       setAssessments(response.data);
     } catch (error) {
       setError(error.message);
@@ -39,30 +35,32 @@ const Assessments = () => {
     getAssessments();
   }, []);
 
-  const getSummary= async () => {
+  const getSummary = async () => {
     const token = localStorage.getItem("token");
-
     const headers = {
       Authorization: `token ${token}`,
     };
-    // console.log(headers)
+
     try {
       setLoading(true);
-      const response = await api.post(`v1/summary/`,{prompt:{assessments}}, {headers});
-      console.log(response.data.response_content);
-      // console.log(assessments)
+      const response = await api.post(
+        `v1/summary/`,
+        {prompt:{assessments}},
+        { headers }
+      );
       setSummarized(response.data.response_content);
     } catch (error) {
       setError(error.message);
     } finally {
-      setLoading(false);  
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    getSummary();
-  }, []);
-
+    if (Object.keys(assessments).length > 0) {
+      getSummary();
+    }
+  }, [assessments]);
 
   return (
     <>
@@ -75,22 +73,22 @@ const Assessments = () => {
               <Card.Title>Assessment Summary</Card.Title>
               <Card.Body>
                 <h3>Neuro</h3>
-                {summarized["neuro"].summary}
-              <br/>
+                {summarized && summarized.neuro && summarized.neuro.summary}
+                <br />
                 <h3>Cardiac:</h3>
-                {summarized.cardiac.summary}
-              <br/>
-              <h3>Respiratory:</h3>
-              {summarized.respiratory.summary}
-              <br/>
-              <h3>GI:</h3>
-              {summarized.GI.summary}
-              <br/>
-              <h3>GU:</h3>
-              {summarized.GU.summary}
-              <br/>
-              <h3>Careplan Recommendation:</h3>
-              {summarized["careplan Recommendation"]}
+                {summarized && summarized.cardiac && summarized.cardiac.summary}
+                <br />
+                <h3>Respiratory:</h3>
+                {summarized && summarized.respiratory && summarized.respiratory.summary}
+                <br />
+                <h3>GI:</h3>
+                {summarized && summarized.GI && summarized.GI.summary}
+                <br />
+                <h3>GU:</h3>
+                {summarized && summarized.GU && summarized.GU.summary}
+                <br />
+                <h3>Careplan Recommendation:</h3>
+                {summarized && summarized.careplan&& summarized.careplan}
               </Card.Body>
             </Card>
           </div>
