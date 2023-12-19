10620cddbc2d1bdaf90c85b923831fc70dd72f28
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2023-12-19 12:41:38 -0500

    added sex field to patient model

--- backend/patient_app/models.py
+++ backend/patient_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Patient(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    sex=models.CharField(max_length=50, blank= True, null=True)
     date_of_birth = models.DateField()
     allergies = models.TextField(blank=True, null=True)
     past_medical_history = models.TextField(blank=True, null=True)
