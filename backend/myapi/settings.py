e9b82dab4f32c650e5bed79ae588d8c2c91caba3
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2024-01-25 21:52:59 -0500

    settings base dir

--- backend/myapi/settings.py
+++ backend/myapi/settings.py
@@ -15,8 +15,11 @@ from dotenv import dotenv_values
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
