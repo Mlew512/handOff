2f2d4d0d8e8e08dac597f5e30ec0ff34625099cc
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2024-01-26 10:36:07 -0500

    debug true

--- backend/myapi/settings.py
+++ backend/myapi/settings.py
@@ -29,7 +29,7 @@ env = dotenv_values(".env")
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -136,11 +136,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
